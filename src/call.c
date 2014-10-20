f7b4cf70cac24121c82882f1cfcef25cd4c8f1e3
digitallysavvy <hermes@digitally-savvy.com>
2014-10-19 23:57:13 -0400

    Call Screen Updated
    The call screen fakes a timer for user testing.

--- src/call.c
+++ src/call.c
@@ -7,8 +7,10 @@ static Window *call_window;
 static TextLayer *call_text_layer;
 static TextLayer *call_timer_layer;
 
+///////////////////////////////////
+// Watchface Methods
+///////////////////////////////////
 
-//Timer
 //Update Time
 static void update_timer() {
   // Get a tm structure
@@ -18,11 +20,16 @@ static void update_timer() {
   // Create a long-lived buffer
   static char buffer[] = "00:00";
 
-  // Write the current hours and minutes into the buffer
-  //Use 12 hour format
-  strftime(buffer, sizeof("00:00"), "%M:%S", tick_time);
-  if(tick_time->tm_second > 10){
-    text_layer_set_text(s_twelvehour_layer, "PM");
+  // Write the current seconds into the buffer
+  strftime(buffer, sizeof("00:00"), "00:%S", tick_time);
+  
+  if(tick_time->tm_sec > 12){
+    text_layer_set_font(call_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+    text_layer_set_text(call_text_layer, "Connected");
+  }
+  else{
+    text_layer_set_font(call_text_layer, fonts_get_system_font(FONT_KEY_BITHAM_30_BLACK));  
+    text_layer_set_text(call_text_layer, "Calling...");
   }
   
 
@@ -30,26 +37,39 @@ static void update_timer() {
   text_layer_set_text(call_timer_layer, buffer);
 }
 
+
+
+
+
 ///////////////////////////////////
 // Window Methods
 ///////////////////////////////////
 
 //window elements
 static void call_window_load(Window *window){
-  // Window and Fram Layers for use by motion discs
+  // Window Layer
   Layer *window_layer = window_get_root_layer(window);
   
-  // Create Confirm TextLayer
-  call_text_layer = text_layer_create(GRect(10, 45, 144, 130));
+  // Create CAll TextLayer
+  call_text_layer = text_layer_create(GRect(0, 45, 144, 130));
   text_layer_set_background_color(call_text_layer, GColorClear);
   text_layer_set_text_color(call_text_layer, GColorBlack);
   text_layer_set_text(call_text_layer, "Calling...");
   text_layer_set_font(call_text_layer, fonts_get_system_font(FONT_KEY_BITHAM_30_BLACK));
-  text_layer_set_text_alignment(call_text_layer, GTextAlignmentLeft);
+  text_layer_set_text_alignment(call_text_layer, GTextAlignmentCenter);
+  
+  // Create time TextLayer
+  call_timer_layer = text_layer_create(GRect(5, 75, 144, 80));
+  text_layer_set_background_color(call_timer_layer, GColorClear);
+  text_layer_set_text_color(call_timer_layer, GColorBlack);
+  text_layer_set_font(call_timer_layer, fonts_get_system_font(FONT_KEY_BITHAM_30_BLACK));
+  text_layer_set_text_alignment(call_timer_layer, GTextAlignmentCenter);text_layer_set_text(call_timer_layer, "00:00");
   
   
   // Add child layers to the window's root layer
   layer_add_child(window_layer, text_layer_get_layer(call_text_layer));
+  layer_add_child(window_layer, text_layer_get_layer(call_timer_layer));
+  update_timer();
 }
 
 // Memory Management
@@ -60,6 +80,12 @@ static void call_window_unload(Window *window){
 
 }
 
+// Tick Handler
+static void call_tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_timer();
+}
+
+// Init Call Window
 static void callInit(){
   
   // Create main Window element and assign to pointer
@@ -70,6 +96,9 @@ static void callInit(){
     .load = call_window_load,
     .unload = call_window_unload
   });
+  
+  // Register with TickTimerService
+  tick_timer_service_subscribe(SECOND_UNIT, call_tick_handler);
   
   // Show the Window on the watch, with animated=true
   window_stack_push(call_window, true);
